33883bf9ec57815a6d7f481118b5ae804c45452d
Ahmed Mahmoud <ahmedmahmo431@gmail.com>
2026-04-26 23:29:06 +0300

    applying hybrid approach for system reset with 2-FF reset synchronizer

--- design0/src/crypto16_coprocessor.vhd
+++ design0/src/crypto16_coprocessor.vhd
@@ -11,7 +11,8 @@ end entity;
 architecture behavioral of crypto16_coprocessor is
 signal ctrl_temp, Rd_temp: std_logic_vector (3 downto 0);
 signal ABus, BBus, Result: std_logic_vector (15 downto 0);
-signal wrt: std_logic;
+signal wrt: std_logic := '0';
+signal res_ff1, res_ff2, res_hybrid: std_logic := '1';
 begin
 	-- Combinational logic declaration
 	combinational: entity combinational
@@ -19,22 +20,36 @@ begin
 	
 	-- Register file declaration
 	register_file: entity register_file
-		port map(clk, res, wrt, Ra, Rb, Rd_temp, Result, ABus, BBus);
-		
-	process (clk) is
+		port map(clk, res_hybrid, wrt, Ra, Rb, Rd_temp, Result, ABus, BBus);
+
+	-- Hybrid reset generator: asynchronous assertion, synchronous deassertion
+	process (clk, res) is
 	begin
-		if(rising_edge(clk)) then
-			if(res = '1') then
-				ctrl_temp <= (others => '0');
-	  			Rd_temp <= (others => '0');
+		if(res = '1') then
+			res_ff1 <= '1';
+			res_ff2 <= '1';
+		elsif(rising_edge(clk)) then
+			res_ff1 <= '0';
+			res_ff2 <= res_ff1;
+		end if;
+	end process;
+
+	res_hybrid <= res_ff2;
+		
+	-- Input register
+	process (clk, res_hybrid) is
+	begin
+		if(res_hybrid = '1') then
+			ctrl_temp <= (others => '0');
+	 		Rd_temp <= (others => '0');
+			wrt <= '0';
+		elsif(rising_edge(clk)) then
+			Rd_temp <= Rd;
+			ctrl_temp <= ctrl;
+			if(ctrl = "0111") then
+				wrt <= '0';
 			else
-				Rd_temp <= Rd;
-				ctrl_temp <= ctrl;
-				if(ctrl = "0111") then
-					wrt <= '0';
-				else
-					wrt <= '1';		 
-				end if;
+				wrt <= '1';		 
 			end if;
 		end if;
 	end process;
